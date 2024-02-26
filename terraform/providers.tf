4565f8df5a6763e838f9335611519fb251dbaa18
jonlee <lee.jonathan@hey.com>
2024-02-26 23:40:33 +0800

    Migrate workspace state to S3

--- terraform/providers.tf
+++ terraform/providers.tf
@@ -7,9 +7,17 @@ terraform {
       version = "5.35.0"
     }
   }
+
+  backend "s3" {
+    region = "ap-southeast-1"
+
+    bucket = "jl-terraform-remote-state-store"
+    key    = "bball8bot/terraform.tfstate"
+
+    dynamodb_table = "terraform_state_lock"
+  }
 }
 
 provider "aws" {
-  region  = "ap-southeast-1"
-  profile = "bball8bot-admin"
+  region = "ap-southeast-1"
 }
