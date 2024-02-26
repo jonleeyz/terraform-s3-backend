96c1fb8bc25ac2fa74a552fca46c865349ab2f57
jonleeyz <32831680+jonleeyz@users.noreply.github.com>
2024-02-26 21:45:52 +0800

    Provision DynamoDB table for Terraform state locking (#5)

--- terraform/main.tf
+++ terraform/main.tf
@@ -8,3 +8,14 @@ resource "aws_s3_bucket_versioning" "terraform_state_store" {
     status = "Enabled"
   }
 }
+
+resource "aws_dynamodb_table" "terraform_state_lock" {
+  name         = "terraform_state_lock"
+  billing_mode = "PAY_PER_REQUEST"
+  hash_key     = "LockID"
+
+  attribute {
+    name = "LockID"
+    type = "S"
+  }
+}
