6e220c206a1cee1569ffdec7997f376f6f45906a
jonlee <lee.jonathan@hey.com>
2024-02-29 13:04:02 +0800

    Add outputs for TF state management infra

--- terraform/s3_state_store.tf
+++ terraform/s3_state_store.tf
@@ -19,3 +19,13 @@ resource "aws_dynamodb_table" "terraform_state_lock" {
     type = "S"
   }
 }
+
+output "state_store_bucket_name" {
+  description = "The bucket name of the account-wide Terraform state store S3 bucket"
+  value = aws_s3_bucket.terraform_state_store.bucket
+}
+
+output "state_lock_table_name" {
+  description = "The bucket name of the account-wide Terraform state locking DynamoDB table"
+  value = aws_dynamodb_table.terraform_state_lock.name
+}
