85786be31cc5d09f55ff418d996f99feb0a0791d
jonlee <lee.jonathan@hey.com>
2024-02-29 16:40:56 +0800

    Add descriptions to IAM role and policies

--- terraform/iam.tf
+++ terraform/iam.tf
@@ -3,15 +3,15 @@ resource "aws_iam_policy" "terraform_state_management" {
   path   = "/"
   policy = data.aws_iam_policy_document.terraform_state_management_policy.json
 
-  lifecycle {
-    prevent_destroy = true
-  }
+  description = "This policy allows the user / role to manage Terraform state for the accountWideTerraformSupport workspace."
 }
 
 resource "aws_iam_policy" "repo_infra" {
   name   = "accountWideTerraformSupport_InfraPolicy"
   path   = "/"
   policy = data.aws_iam_policy_document.workspace_infra_policy.json
+
+  description = "This policy will be used to manage infrastructure that supports S3 Terrform state management and associated CI workflows for the entire account."
 }
 
 resource "aws_iam_role" "repo_ci" {
@@ -21,6 +21,8 @@ resource "aws_iam_role" "repo_ci" {
     aws_iam_policy.terraform_state_management.arn,
     aws_iam_policy.repo_infra.arn
   ]
+
+  description = "This role will be used to execute GitHub Actions workflows that will create Terraform plans and apply them."
 }
 
 output "ci_iam_role_arn" {
