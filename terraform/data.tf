776f3d5bf214e6ce24368ce0938e43ed01bcee81
jonlee <lee.jonathan@hey.com>
2024-03-01 14:46:13 +0800

    Update infra IAM policy: Expand scope
    More widespread permissions are required to ensure Terraform can plan and apply.

--- terraform/data.tf
+++ terraform/data.tf
@@ -33,44 +33,9 @@ data "aws_iam_policy_document" "workspace_infra_policy" {
   statement {
     effect = "Allow"
     actions = [
-      "s3:ListBuckets",
-      "s3:CreateBucket",
-      "s3:DeleteBucket"
-    ]
-
-    resources = ["*"]
-  }
-  statement {
-    effect = "Allow"
-    actions = [
-      "dynamodb:ListTables",
-      "dynamodb:DescribeTable",
-      "dynamodb:CreateTable",
-      "dynamodb:DeleteTable"
-    ]
-
-    resources = ["*"]
-  }
-  statement {
-    effect = "Allow"
-    actions = [
-      "iam:CreateOpenIDConnectProvider",
-      "iam:DeleteOpenIDConnectProvider",
-    ]
-
-    resources = [
-      aws_iam_openid_connect_provider.github.arn
-    ]
-  }
-  statement {
-    effect = "Allow"
-    actions = [
-      "iam:CreateRole",
-      "iam:DeleteRole",
-      "iam:CreatePolicy",
-      "iam:DeletePolicy",
-      "iam:AttachRolePolicy",
-      "iam:DetachRolePolicy"
+      "s3:*",
+      "dynamodb:*",
+      "iam:*"
     ]
 
     resources = ["*"]
