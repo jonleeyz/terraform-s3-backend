9687c9d3a80d7ffdcd197298964bb3ed77419a43
jonlee <lee.jonathan@hey.com>
2024-03-05 12:02:40 +0800

    Add ARN outputs for global state management infra

--- terraform/global_state_management_support.tf
+++ terraform/global_state_management_support.tf
@@ -37,3 +37,19 @@ resource "aws_iam_openid_connect_provider" "github" {
   ]
   thumbprint_list = ["1b511abead59c6ce207077c0bf0e0043b1382612"]
 }
+
+output "terraform_state_store_bucket_arn" {
+  value       = aws_s3_bucket.terraform_state_store.arn
+  description = "The ARN of the provisioned S3 bucket that will store state for all Terraform workspaces associated with this AWS account"
+}
+
+output "terraform_state_lock_table_arn" {
+  value       = aws_dynamodb_table.terraform_state_lock.arn
+  description = "The ARN of the provisioned DynamoDB table that will perform state locking for all Terraform workspaces associated with this AWS account"
+}
+
+output "oidc_iam_provider_arn" {
+  value       = aws_dynamodb_table.terraform_state_lock.arn
+  description = "The ARN of the provisioned OpenID Connect IAM Provider that will allow requests from GitHub to assume IAM roles"
+}
+
